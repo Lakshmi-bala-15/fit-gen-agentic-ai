8c02af07b3687883c68689956e931a24fec8a29f
Lakshmi-bala-15 <lakshmibala.b.bme.2023@snsct.org>
2026-04-08 14:39:21 +0530

    Create intake_agent.py

---
+++ agents/intake_agent.py
@@ -0,0 +1,11 @@
+def get_user_profile():
+    user = {
+        "age": 22,
+        "height": 165,
+        "weight": 60,
+        "goal": "fat_loss",
+        "diet": "vegetarian",
+        "activity": "moderate",
+        "restrictions": ["no eggs"]
+    }
+    return user
